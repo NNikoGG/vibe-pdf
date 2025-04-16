9bbc3e40df4fc9dfbbdde5baa9ef0b7a11e22f23
NNikoGG <geekgaming222@gmail.com>
2025-04-17 00:38:10 +0530

    add zod schema validation

--- components/upload/upload-form.tsx
+++ components/upload/upload-form.tsx
@@ -1,12 +1,39 @@
 "use client";
 
+import { z } from "zod";
 import UploadFormInput from "./upload-form-input";
 
-const handleSubmit = () => {
-  console.log("submit");
-};
+const schema = z.object({
+  file: z
+    .instanceof(File, { message: "Invalid file" })
+    .refine(
+      (file) => file.size <= 20 * 1024 * 1024,
+      "File size must be less than 20MB"
+    )
+    .refine(
+      (file) => file.type.startsWith("application/pdf"),
+      "File must be a PDF"
+    ),
+});
 
 export default function UploadForm() {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("submitted");
+    const formData = new FormData(e.currentTarget);
+    const file = formData.get("file") as File;
+
+    const validatedFields = schema.safeParse({ file });
+    console.log(validatedFields);
+
+    if (!validatedFields.success) {
+      console.log(
+        validatedFields.error.flatten().fieldErrors.file?.[0] ?? "Invalid file"
+      );
+      return;
+    }
+  };
+
   return (
     <div className="flex flex-col gap-8 w-full max-w-2xl mx-auto">
       <UploadFormInput onSubmit={handleSubmit} />
