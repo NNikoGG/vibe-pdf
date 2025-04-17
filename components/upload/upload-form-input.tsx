ac9b85955af5bca8a322e46ea73a8f38eeeaf69d
NNikoGG <geekgaming222@gmail.com>
2025-04-18 01:43:08 +0530

    add openai system prompt and summary generation

--- components/upload/upload-form-input.tsx
+++ components/upload/upload-form-input.tsx
@@ -2,26 +2,44 @@
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import React from "react";
+import { cn } from "@/lib/utils";
+import { Loader2 } from "lucide-react";
+import React, { forwardRef } from "react";
 
 interface UploadFormInputProps {
   onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+  isLoading: boolean;
 }
 
-export default function UploadFormInput({ onSubmit }: UploadFormInputProps) {
-  return (
-    <form className="flex flex-col gap-6" onSubmit={onSubmit}>
-      <div className="flex justify-end items-center gap-1.5">
-        <Input
-          type="file"
-          id="file"
-          name="file"
-          accept="application/pdf"
-          required
-          className=""
-        />
-        <Button>Upload your pdf</Button>
-      </div>
-    </form>
-  );
-}
+const UploadFormInput = forwardRef<HTMLFormElement, UploadFormInputProps>(
+  ({ onSubmit, isLoading }, ref) => {
+    return (
+      <form className="flex flex-col gap-6" onSubmit={onSubmit} ref={ref}>
+        <div className="flex justify-end items-center gap-1.5">
+          <Input
+            type="file"
+            id="file"
+            name="file"
+            accept="application/pdf"
+            required
+            className={cn(isLoading && "opacity-50 cursor-not-allowed")}
+            disabled={isLoading}
+          />
+          <Button disabled={isLoading}>
+            {isLoading ? (
+              <>
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Processing...
+              </>
+            ) : (
+              "Upload your PDF"
+            )}
+          </Button>
+        </div>
+      </form>
+    );
+  }
+);
+
+UploadFormInput.displayName = "UploadFormInput";
+
+export default UploadFormInput;
