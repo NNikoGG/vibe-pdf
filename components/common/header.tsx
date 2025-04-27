8b710f98a7deddfdd95e8ee01a26c3575bf5f0a2
NNikoGG <geekgaming222@gmail.com>
2025-04-28 00:09:58 +0530

    update readme

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,4 +1,4 @@
-import { FileText } from "lucide-react";
+import { FileHeart } from "lucide-react";
 import NavLink from "./nav-link";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import PlanBadge from "./plan-badge";
@@ -8,7 +8,7 @@ export default function Header() {
     <nav className="container flex items-center justify-between py-4 lg:px-8 px-2 mx-auto">
       <div className="flex lg:flex-1">
         <NavLink href="/" className="flex items-center gap-1 lg:gap-2 shrink-0">
-          <FileText className="h-5 w-5 lg:h-8 lg:w-8 text-gray-900 hover:rotate-12 transform transition duration-200 ease-in-out" />
+          <FileHeart className="h-5 w-5 lg:h-8 lg:w-8 text-gray-900 hover:rotate-12 transform transition duration-200 ease-in-out" />
           <span className="font-extrabold lg:text-xl text-gray-900">
             VibePDF
           </span>
